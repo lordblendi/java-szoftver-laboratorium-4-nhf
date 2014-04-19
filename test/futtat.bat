6345e8bfd9cf256c3e40f281ecf9d6d1e4348d4f
Marton Sebok <sebokmarton@gmail.com>
2014-04-19 18:52:41 +0200

    Színes teszt kimenet.

--- test/futtat.bat
+++ test/futtat.bat
@@ -12,6 +12,11 @@ echo.
 echo Tesztek futtatasa...
 set count=1
 set sikeres=0
+for /f "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
+  set "DEL=%%a"
+)
+set /p ".=%DEL%" <NUL > SIKERES
+set /p ".=%DEL%" <NUL > HIBA
 for %%f in (*.in.txt) do (
 	set infile=%%~nf.txt
 	set base=!infile:~0,-7!
@@ -30,16 +35,21 @@ for %%f in (*.in.txt) do (
 	diff -u !testfile! !outfile! > !difffile!
 	
 	if ERRORLEVEL 1 (
-		echo 	Eredmeny:	HIBA
+		set /p ".=%DEL% 	Eredmeny:	" <NUL
+		call :Szines 0C HIBA
 	) ELSE (
-		echo 	Eredmeny:	SIKERES
-		set /A sikeres=!sikeres! + 1
+		set /p ".=%DEL% 	Eredmeny:	" <NUL
+		call :Szines 0A SIKERES
+		set /a sikeres=!sikeres! + 1
 	)
+	echo.
 	
 	echo.
-	set /A count=!count! + 1
+	set /a count=!count! + 1
 )
-echo !sikeres!/!count! SIKERES teszteset
+set /p ".=!sikeres!/!count! " <NUL
+call :Szines 0A SIKERES
+echo  teszteset
 echo Nyomj meg egy billentyut miutan megvizsgaltad a kimeneteket! ...
 
 pause >NUL
@@ -53,7 +63,13 @@ echo Takaritas...
 ::del ..\bin\*.class 2>NUL
 del *.out.txt 2>NUL
 del *.diff.txt 2>NUL
+del SIKERES >NUL 2>&1
+del HIBA >NUL 2>&1
 echo.
 goto :EOF
+
+:Szines
+findstr /v /a:%1 /R "^$" "%~2" NUL
+goto :eof
 
 :End
